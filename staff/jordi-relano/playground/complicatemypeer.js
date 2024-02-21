cf20d9170e03b112ea463ca92ea59aa13ec33d90
JordiRZ <jordirz199444@gmail.com>
2024-02-21 09:43:57 +0100

    add my update content #127

---
+++ staff/jordi-relano/playground/complicatemypeer.js
@@ -0,0 +1,25 @@
+const bomb2 = 1500
+const bomb = [50, 100, 500, 100, 2000, 3000, 5000, 10000]
+var destruct = (explosion) => "The objective has been destroyed"
+var implement = (x) => x + 3000
+var implement2 = (y) => y - 500
+const country = {
+    name: "Moldavia",
+    population: "20mil",
+    presindet: "Trt",
+    pws: 8500
+}
+const country2 = {
+    name: "Spain",
+    population: "47mil",
+    presindet: "Marianito",
+    pws: 4500
+}
+
+//Cambia el nombre del presidente de Moldavia por Maia Sandu
+//Extrae la posición 6 del array bomb
+//Busca la manera de tener suficiente potencia en bomb2 para destruir country2 "los pws son los puntos que tiene un país para aguantar una bomba"
+//Llama a la función destruct una vez hayas podido destruir el país
+//Busca la manera de tener suficiente potencia en bomb2 para destruir country "los pws son los puntos que tiene un país para aguantar una bomba"
+
+
