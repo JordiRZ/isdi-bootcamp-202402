68a5292b73590e2568e10e9d273a663edcdbbd4f
JordiRZ <jordirz199444@gmail.com>
2024-03-15 14:45:03 +0100

    add working on stuf #433

---
+++ staff/jordi-relano/testing/prototyping/prototype.js
@@ -0,0 +1,100 @@
+function Transport() {
+    this.moving = false
+}
+
+
+
+
+Transport.prototype.move = function () {
+    this.moving = true
+}
+
+
+
+
+Transport.prototype.stop = function () {
+    this.moving = false
+}
+
+
+
+
+function Bike() {
+}
+
+Bike.prototype = new Transport
+
+function Moto() {
+}
+
+
+Moto.prototype = new Transport
+
+
+function Car() {
+}
+
+Car.prototype = new Transport
+
+var bike = new Bike
+
+var moto = new Moto
+
+var car = new Car
+
+// Transport { moving: false }
+
+car.move()
+
+// undefined
+
+car.moving
+
+// true
+
+car.stop()
+
+// undefined
+
+car.moving
+
+// falsefunction Transport() {
+//     this.moving = false
+// }
+
+Transport.prototype.move = function () {
+    this.moving = true
+}
+
+Transport.prototype.stop = function () {
+    this.moving = false
+}
+
+function Bike() {
+}
+
+Bike.prototype = new Transport
+
+function Moto() {
+}
+
+Moto.prototype = new Transport
+
+function Car() {
+}
+
+Car.prototype = new Transport
+
+
+var bike = new Bike
+var moto = new Moto
+var car = new Car
+// Transport { moving: false }
+car.move()
+// undefined
+car.moving
+// true
+car.stop()
+// undefined
+car.moving
+// false
